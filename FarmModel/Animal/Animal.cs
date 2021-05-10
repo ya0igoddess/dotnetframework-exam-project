768ffdc01bc77c3e1a3471a7bbb7fd80de7caa81
ya0igoddess <matratskingalya@gmail.com>
2021-05-10 14:45:07 +0300

    added counstructor and methods prototypes in animal class

--- FarmModel/Animal/Animal.cs
+++ FarmModel/Animal/Animal.cs
@@ -13,6 +13,34 @@ namespace FarmModel
         public int Weight { get; private set; }
         public AnimalSex Sex { get; private set; }
         public Animals AnimalKind { get; private set; }
+        private double growCoefficient;
+
+        protected Actions.MilkingAction milkingAction;
+        protected Actions.EggsCollectingAction eggsCollectingAction;
+        protected Actions.SheeringAction sheeringAction;
         
+
+        public Animal(AnimalSex sex, int age, int weight)
+        {
+            this.Sex = sex;
+            this.Age = age;
+            this.Weight = weight;
+        }
+
+        public void GrowUp()
+        {
+            Age++;
+            Weight = (int)(Weight * growCoefficient);
+            milkingAction.Refresh();
+            eggsCollectingAction.Refresh();
+            sheeringAction.Refresh();
+        }
+
+        public void ButchUp()
+        {
+            //TODO return meat
+        }
+
+
     }
 }
